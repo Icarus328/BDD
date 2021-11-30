5202d05e347988a2169449196298b31d67ba4567
Sebastian Contreras <sbttn.c@gmail.com>
2021-11-30 12:40:22 -0300

    CSS

--- Sites/index.php
+++ Sites/index.php
@@ -1,34 +1,74 @@
 <html>
-  
-<head>
+<head lang="en" >
   <link href="styles/estilos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php include('templates/header.html'); ?>
 </head>
 
-<body>
-  <form method="POST" action="views/register.php">
-    <input type="submit" value="Registrarse"/>
-  </form>
+<body class="bg-dark" id="page-top">
 
-  <form method="POST" action="views/login.php">
-    <input type="submit" value="Iniciar Sesión"/>
-  </form>
+<section>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand text-warning text-uppercase" href="#page-top">Prime Max</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" href="#ingresar">Ingresar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#aboutus">Acerca de nosotros</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</section>
 
-  <form method="POST" action="views/profile.php">
-    <input type="submit" value="Ver Perfil"/>
-  </form>
+<section class="hero is-success is-halfheight banner text-center">
+  <div class="hero-body" style="height: 70%">
+    <div class="text-center  text-white" style="padding: 7%">
+      <h1 class="text-warning display-1 text-uppercase ">
+        Prime Max
+      </h1>
+      <h2 class="text-white display-6">
+        Bienvenido a la mejor página de videojuegos y películas.
+      </h2>
+    </div>
+    <div class="btn-group w-75 pt-5">
+      <a class="btn btn-outline-warning mx-2 rounded" href="views/main.php" ">Ver main</a>
+      <a class="btn btn-outline-warning mx-2 rounded" href="views/profile.php" >Ver Perfil</a>
+      <a class="btn btn-outline-warning mx-2 rounded" href="views/migraciones.php" ">Ver migraciones</a>
+    </div>
+  </div>
+</section>
 
-  <form method="POST" action="views/main.php">
-    <input type="submit" value="Ver main"/>
-  </form>
+<section class="text-center mt-5" id="ingresar" style="height: 50%">
+  <h2 class="text-white pt-5 display-4 text-uppercase">
+    Accede a Prime Max
+  </h2>
 
-  <form method="POST" action="views/migraciones.php">
-    <input type="submit" value="Ver migraciones"/>
-  </form>
+  <h3 class="text-muted">
+    Inicia sesión o Registrate
+  </h3>
+
+  <div class="btn-group-vertical w-75">
+    <a class="btn btn-warning my-2 rounded" href="views/login.php" >Iniciar Sesión</a>
+    <a class="btn btn-warning my-2 rounded" href="views/register.php"">Registrarse</a>
+  </div>
+</section>
+
+
+<section class="text-center" id="about" style="height: 20%">
+  <h3 class="text-muted">
+  Grupo 77 y Grupo 112
+  </h3>
+  
+</section>
 
 </body>
 
-
-
-
+</html>
 
--- Sites/views/login.php
+++ Sites/views/login.php
@@ -1,40 +1,68 @@
 <html>
-
 <head>
-<link href="../styles/estilos.css" rel="stylesheet">
-  <?php include('../templates/header.html');   ?>
+  <link href="../styles/estilos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include('../templates/header.html');?>
 </head>
 
 
-<body>
-<div class="signup-form">
-  <form action="../controllers/login_controller.php" method="post">
-		<h2>Inicio de Sesión</h2>
-		<p>Ingresa tu correo y contraseña</p>
-    <div class="form-group">
-	    <div class="row">
-		    <div class="col">
-          <input type="email" name="email" placeholder="Corre electrónico" required="required">
-         </div>
-			  <div class="col">
-          <input type="password" name="password" placeholder="Contraseña" required="required">
-        </div>
+<body class="bg-dark text-white">
+
+<section>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-black navbar-shrink">
+    <div class="container-fluid">
+      <a class="navbar-brand text-warning text-uppercase" href="../index.php">Prime Max</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" href="../index.php#ingresar">Ingresar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../index.php#about">Acerca de nosotros</a>
+          </li>
+        </ul>
       </div>
     </div>
-    <br>
-		<div class="form-group">
+  </nav>
+</section>
+
+
+<section class="text-center mt-5" id="ingresar" style="height: 30%">
+  <h1 class="text-white pt-5 display-4 text-uppercase">
+    Iniciando Sesión
+  </h1>
+  <h2 class="text-muted">
+    ¿Cuales son sus credenciales?
+  </h2>
+
+</section>
+
+<section class="text-center" style="height: 40%; padding-left: 10%; padding-right: 10%">
+  <form action="../controllers/login_controller.php" method="post">
+    <div class="input-group mb-3" >
+      <input type="email" name="email" class="form-control" placeholder="Correo electrónico" required="required">
+    </div>
+
+    <div class="input-group mb-3">
+      <input type="password" name="password" class="form-control" placeholder="Contraseña" required="required">
+    </div>
+
+    <div class="form-group">
       <input type="submit" name="save" value="Iniciar Sesión"></input>
     </div>
-    <br>
-    <div class="text-center">Aún  no tienes una cuenta?
+
+    <div class="text-center mt-5">Aún  no tienes una cuenta?
       <a href="register.php">Registrate</a>
     </div>
-  </form>
-</div>
 
-  <form method="POST" action="../index.php">
-    <input type="submit" value="Volver"/>
   </form>
+</section>
+
+<div class="text-center mt-5" id="ingresar">
+  <a class="btn btn-outline-warning my-2 rounded w-75" href="../index.php">Volver</a>
+</div>
 
 </body>
 </html>
